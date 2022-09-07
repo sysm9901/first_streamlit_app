e0569f28ecba8b8ce7858065dde47de26ce70280
sysm9901 <112796119+sysm9901@users.noreply.github.com>
2022-09-06 17:19:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@
 import streamlit
 
 streamlit.title("My parent's New Healthy Diner")
-
-
-                
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamit.text("Hard-boiled Free-range egg")
